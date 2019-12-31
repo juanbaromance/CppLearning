9441076d2163882d22b47327ec0755beeb41b461
juanba <juanba.romance@gmail.com>
2019-12-31 18:15:46 +0100

    one line coding on c++17

--- pre_learning/mixin/main.cpp
+++ pre_learning/mixin/main.cpp
@@ -30,11 +30,36 @@ public:
 	}
 };
 
-
+#include <tuple>
+#include <array>
+#include <map>
 int main()
 {
 	Name pp("Pepito", "Perez");
 	pp.dump();
 
 	RepeatPrint<Name>(pp).repeat(3);
+
+    // one line coding smell(lack of)
+    // c++17 binding capabilites
+    using namespace std;
+
+    auto t = make_tuple(1.0, "Ferchi");
+    auto & [ number, name ] = t;
+    cout << __PRETTY_FUNCTION__ << " : " << name << "." << number << endl;
+
+    array<int,3> v = {1,2,3};
+    auto & [ x, y, z ] = v;
+    cout << __PRETTY_FUNCTION__ << "(" << x << " " << y << " " << z << ")" << endl;
+
+    int constexpr  index = 00;
+    auto m = map<int,const char*>( { {0,"Fulanito"}, {1,"Menganito"} } );
+    if( auto i = m.find(index); i == m.end() )
+    {
+        cout << "Missed key(" << index << ")" << endl;
+    }
+    else
+    {
+        cout << "Got " << i->second << endl;
+    }
 }
